2f32d07e49dda3966620bf53346952ea17ff4c30
Pas_M.Prima <118792179+PrimaWira@users.noreply.github.com>
2023-03-14 09:45:03 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,16 @@
-let nilai = prompt("Masukam nilai : " , 0);
-let hasil = "";
-
-switch(true){
-    case nilai >= 91 && nilai <= 100 :
-        hasil = "A"
-        break;
-    case nilai >= 81 && nilai <= 90 :
-        hasil = "B"
-        break;
-    case nilai >= 71 && nilai <= 80:
-        hasil = "C"
-        break;
-    case nilai >= 61 && nilai <= 70:
-        hasil = "D"
-        break;
-    case nilai >= 51 && nilai <= 60:
-        hasil = "E"
-        break;
-        case nilai >= 0 && nilai <= 50:
-            hasil = "F"
-            break; 
-            case nilai >= -100 && nilai <= -1:
-                hasil = "kekurangan bro"
-                break;  
-        default:
-            document.write("kelebihan bro");
+const users =[{
+    Brand: 'tesla',
+    BBM: 'Pertamax',
+    type: 'mobil listrik',
+},
+{
+    Brand: 'Supra',
+    BBM: 'pertamax Turbo',
+    type: 'sport car',
 }
-document.write(hasil);
+]
+
+let type = prompt ("masukan type")
+const usersType = users.filter((user) => user.type === type);
+
+console.log(usersType)
